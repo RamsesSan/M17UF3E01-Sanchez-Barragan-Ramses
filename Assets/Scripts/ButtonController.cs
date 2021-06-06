7a1f6208e034f4db8acd0de3f757d26bf117c4de
Cacaillo <ramses.sanchez.7e2@itb.cat>
2021-06-07 01:59:57 +0200

    Initial commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour{
+
+    private SpriteRenderer sprtRender;
+    public Sprite defaultImage;
+    public Sprite pressedImage;
+
+    public KeyCode keyToPress;
+    // Start is called before the first frame update
+    void Start(){
+        sprtRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            sprtRender.sprite = pressedImage;
+        }
+        if (Input.GetKeyUp(keyToPress))
+        {
+            sprtRender.sprite = defaultImage;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# M17UF3E01-Sanchez-Barragan-Ramses
+ mi proyecto mejorado
